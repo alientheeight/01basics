3312234ef43f81564803b7491cc456a99fc759f5
Tomasz Czapski <czapski.tomasz@hotmail.com>
2021-09-16 09:08:26 +0200

    Heder component changed from funcional to class type component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<header>
+				<div>Logo</div>
+				<input />
+			</header>
+		);
+	}
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  )
-}
+import Header from './components/header';
 
-ReactDOM.render(<App/> , document.getElementById('root'));
+const App = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
